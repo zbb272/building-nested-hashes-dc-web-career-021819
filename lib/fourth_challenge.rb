dd50579a54906119639a177c3a934b370fb05a91
Zachary <zbberman@yahoo.com>
2019-02-04 17:28:48 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,8 +5,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {:name => "Benvolio", :age => 17, :attitude => "worried"},
-        {:name => "Mercutio", :age => 18, :attitude => "hot-headed"}
+        {:name => "Benvolio", :age => "17", :attitude => "worried"},
+        {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
       ]
    }, 
    :capulet => {
@@ -14,8 +14,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        {:name => "Steven", :age => 30, :attitude => "confused"},
-        {:name => "Nurse", :age => 44, :attitude => "worried"}
+        {:name => "Steven", :age => "30", :attitude => "confused"},
+        {:name => "Nurse", :age => "44", :attitude => "worried"}
       ]
    }
 }
